952b942e392ec865f21709c0c08ff851ce7d194d
TRẦN MINH CƯỜNG <41182580+MinhCuongIT@users.noreply.github.com>
2019-07-14 11:40:39 +0700

    Revert "Check null truoc khi send message"
    This reverts commit d9ab7fcabf6ad51787880495893d704bee9f9770.

--- services/config.js
+++ services/config.js
@@ -1,5 +1,4 @@
 export const config = {
-  // baseURL: 'http://192.168.1.9:5500/'
+  baseURL: 'http://192.168.1.9:5500/'
   // baseURL:'https://carehealth-242817.appspot.com/'
-  baseURL: 'http://54.179.155.241/'
 }
