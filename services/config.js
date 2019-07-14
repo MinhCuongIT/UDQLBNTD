e6e97f15737b892421698ce5c67a0092c4347662
dhgcat010397 <1512036@student.hcmus.edu.vn>
2019-07-14 13:35:15 +0700

    fix lỗi install app trên android 9.0

--- services/config.js
+++ services/config.js
@@ -1,5 +1,5 @@
 export const config = {
-  // baseURL: 'http://192.168.1.9:5500/'
+  baseURL: 'http://192.168.1.9:5500/'
   // baseURL:'https://carehealth-242817.appspot.com/'
-  baseURL: 'http://54.179.155.241/'
+  //baseURL: 'http://54.179.155.241/'
 }
